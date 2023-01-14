895d09f74cda8f2005fc50d5154fd40e80f64d87
petehall6 <peter.hall6@gmail.com>
2023-01-13 23:19:22 -0600

    basic loc[] works again
    Need to implement some sort of approximate match.  Possibly grep through and grab wildcard matches to list

--- Copy-number/csv_to_h5.py
+++ Copy-number/csv_to_h5.py
@@ -10,16 +10,22 @@ h5store = "copy_number_processed.h5"
 #open large csv via chunking
 chunk = pd.read_csv('copyNumber.csv', chunksize=10000)
 df = pd.concat(chunk)
-df = df.rename(columns={"cell_line_display_name": "cell_line"})
-df.set_index("cell_line", drop=False)
+df.astype(str)
 print(df.tail())
+df = df.rename(columns={"cell_line_display_name": "cell_line"})
+
+df.set_index("cell_line", drop=True, inplace=True)
+print(df.tail())
+
+
 #convert to h5 file for quicker parsing
 df.to_hdf(h5store,key='df', mode='w')
 print("H5 file created\n\n")
 
 df = pd.read_hdf(h5store,"df")
 
-print(df.tail())
+
+
 
 
 
--- Copy-number/findCopyNum.py
+++ Copy-number/findCopyNum.py
@@ -6,7 +6,16 @@ data_store = "copy_number_processed.h5"
 
 df = pd.read_hdf(data_store)
 
-print(df.head())
+#strips hyphens for easier parsing.  Trying to remove hyphen in the hdf5 will give an unique index error
+#stripping here does not impact performance
+df.astype(str)
+df.columns= df.columns.str.replace("-","")
+
+cell_line = "143B"
+gene = "ADA"
+
+print(df.loc[cell_line,gene])
+print(df.get(gene, default='Cell not found'))
 
 
 
