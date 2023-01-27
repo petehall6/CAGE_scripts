956eda8621d20163f6f0ad3b6a10d976c67f5885
petehall6 <peter.hall6@gmail.com>
2023-01-27 16:02:19 -0600

    conditional formatting
    conditional formating SNP test

--- NGS_auto_format/excel_filter_test.py
+++ NGS_auto_format/excel_filter_test.py
@@ -1,16 +1,42 @@
 import pandas as pd
 
-wb = pd.ExcelFile("exl_filter.xlsx")
+wb = pd.ExcelFile("raw_xl.xlsx")
 df = wb.parse("Sheet1")
 
+def sort_indel(df):
+    df = df.sort_values(by="Total_indel",ascending=False)
+    #explicitly cast total indel column as float for percentages
+    df['Total_indel'] = df['Total_indel'].astype(float).map('{:.3%}'.format)
+    writer = pd.ExcelWriter("sorted_xl.xlsx")
+    df.to_excel(writer)
+    writer.save()
 
-df = df.sort_values(by="Total_indel",ascending=False)
-#explicitly cast total indel column as float for percentages
-df['Total_indel'] = df['Total_indel'].astype(float).map('{:.3%}'.format)
 
 
-df.style.apply(lambda x: ["background: red" if v > 1.03 else "" for v in df['SNP_test']], axis = 1)
-print()
-writer = pd.ExcelWriter("sorted_xl.xlsx")
-df.to_excel(writer)
+
+
+
+                    
+df.style.apply(lambda x: ["background-color: red" 
+                          if (i == 23 and (v > 1.03)) 
+                          else "background-color: blue" for i, v in enumerate(x)], axis = 1)
+                    
+                    
+                    
+
+writer = pd.ExcelWriter("sorted_xl.xlsx", engine='xlsxwriter')
+df.to_excel(writer, sheet_name='Sheet1')
+wb = writer.book
+ws = writer.sheets['Sheet1']
+snp_format = wb.add_format({'bg_color': 'red'})
+
+ws.conditional_format('W2:W341',
+                      {'type': 'cell',
+                       'criteria': '>',
+                       'value': '1.03',
+                       'format': snp_format})
+
 writer.save()
+
+
+
