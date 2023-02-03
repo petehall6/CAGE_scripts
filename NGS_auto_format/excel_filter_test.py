05b2fcacc58b9c74b13d78f9cade8e66511088fe
petehall6 <peter.hall6@gmail.com>
2023-02-03 08:14:04 -0600

    2/3

--- NGS_auto_format/excel_filter_test.py
+++ NGS_auto_format/excel_filter_test.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import os
+
+
+fpath = "C:\\Users\\phall3\\Documents\\repos\\CAGE_scripts\\NGS_auto_format\\sorted_xl.xlsx"
 
 wb = pd.ExcelFile("raw_xl.xlsx")
 df = wb.parse("Sheet1")
@@ -17,30 +21,27 @@ df = df.sort_values(by="Total_indel",ascending=False)
 #explicitly cast total indel column as float for percentages
 df['Total_indel'] = df['Total_indel'].astype(float).map('{:.3%}'.format)
 
+#TODO split indel reads into separate columns for easier sorting?
 
-indel_format = wb.add_format({'bg_color':'pink'})
+snp_out_format = wb.add_format({'bg_color':'green'})
 
-snp_format = wb.add_format({'bg_color': 'red'})
 
-snp_condition = ws.conditional_format('W2:W341',
-                      {'type': 'cell',
-                       'criteria': '>',
-                       'value': '1.03',
-                       'format': snp_format})
 
-#Indel columns: G,I,K,M,O,Q,s,U
-#=MOD(COLUMN(),3)=0
+#out of frame indels in reads 1 and 2 and SNP between 0.98 and 1.03 are highlighted 3
+big_conditional = ws.conditional_format('A2:Y341',
+                      {'type': 'formula',
+                       'criteria': '=AND($W2>0.98,$W2<1.03,ABS(MOD($G2,3))<>0,$G2<>0)',
+                       'format': snp_out_format})
 
-in_frame = ws.conditional_format('G2:G385',
-                                 {'type': 'formula',
-                                  'criteria': '=AND(ABS(MOD($G2,3))=0,$G2<>0)',
-                                  'format': indel_format})
+
 
 df.to_excel(writer)
 
 writer.save()
-
+#for os open
+writer.close()
 print("Format complete")
+os.system(fpath)
 
 
 
