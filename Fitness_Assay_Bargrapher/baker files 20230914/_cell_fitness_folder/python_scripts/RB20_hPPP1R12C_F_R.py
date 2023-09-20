19892fed3e0668be8275c081a516eb9c5a27e059
petehall6 <peter.hall6@gmail.com>
2023-09-20 08:08:57 -0500

    CelFi Baker-20230915

---
+++ Fitness_Assay_Bargrapher/baker files 20230914/_cell_fitness_folder/python_scripts/RB20_hPPP1R12C_F_R.py
@@ -0,0 +1,26 @@
+import os,sys,inspect
+import argparse
+
+def main():
+    ID = str(os.path.basename(__file__)).split('.py')[0]
+    the_Seq = str.upper('CCAACCCGGGCCCCTATGTCCACTTCAGGACAGCATGTTTGCTGCCTCCAGGGATCCTGTGTCCCCGAGCTGGGACCACCTTATATTCCCAGGGCCGGTTAATGTGGCTCTGGTTCTGGGTACTTTTATCTGTCCCCTCCACCCCACAGTGGGGCCACTAGGGACAGGATTGGTGACAGAAAAGCCCCATCCTTAGGCCTCCTCCTTCCTAGTCTCCTGATATTGGGTCTAACCCCCACCTCCTGTTAGGCAGATTCCTTATCTGGTGACACACCCCCATTTCCTGGAGCCATCTCTCTCCTTGCCAGAACCTCTAAGGTTTGCTTACGATGGAGCCAGAGAGGATCCTGGGAGGGAGAGCTTGGCAGG')
+    the_Seq_start = str.upper('CTCCAGGGATCCT')
+    the_Seq_end = str.upper('CTCTAAGGTTTGC')
+    fastq_files = '/research_jude/rgs01_jude/groups/millergrp/projects/CrisprDesigns/common/analysis/082223/fastq/joined/*.fastq'
+    test_list = [str("g2"), str.upper("ACCCCACAGTGGGGCCACTAGGG")
+				]
+
+
+
+    search_fastq(ID,the_Seq,the_Seq_start,the_Seq_end,fastq_files,test_list)
+
+
+
+
+
+if __name__ =='__main__':		# use this if you want to include modules from a subfolder
+    cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0], r'Z:\ResearchHome\ResearchHomeDirs\millergrp\common\Python\CAGE_Programs\NGS_one_main_program')))
+    if cmd_subfolder not in sys.path:
+        sys.path.insert(0, cmd_subfolder)
+        from clone_check_main import search_fastq
+    main()
