56d8bd89cd0a5908419b978177ab20030ddfcc15
petehall6 <peter.hall6@gmail.com>
2024-03-27 13:53:13 -0500

    v1 of all_indel_batching

---
+++ all_indels_batching/holding/CAGE722_hDNMT3A_F_R_all_indels.py
@@ -0,0 +1,25 @@
+
+#TODO make multi_range template
+
+
+import os,sys,inspect
+import argparse
+
+def main():
+    ID = str(os.path.basename(__file__)).split('.py')[0]
+    the_Seq = str.upper('TGTGAGTGCCCCTGTCCCTGCACTTCGGGTGGCTGCTGGTCCTCCGGGTCCTGCTGTGTGGTTAGACGGCTTCCGGGCAGCCTGGTCTGGCCAGCACTCACCCTGCCCTCTCTGCCTTTTCTCCCCCAGGGTATTTGGTTTCCCAGTCCACTATACTGACGTCTCCAACATGAGCCGCTTGGCGAGGCAGAGACTGCTGGGCCGGTCATGGAGCGTGCCAGTCATCCGCCACCTCTTCGCTCCGCTGAAGGAGTATTTTGCGTGTGTGTAAGGGACATGGGGGCAAACTGAGGTAGCGACACAAAGTTAAACAAACAAACAAAAAACACAAAACATAATAAAACACCAAGAACATGAGGATGGAGAGAAGTATCAGCACCCA')
+    the_Seq_start = str.upper('GGGTCCTGCTGTG')
+    the_Seq_end = str.upper('AAACACAAAACAT')
+    fastq_files = 'Miller-Plate3[0-2]*.fastq'
+    test_list = [str("g8"), str.upper("CCGCTTGGCGAGGCAGAGACTGC")]
+    
+    search_fastq(ID,the_Seq,the_Seq_start,the_Seq_end,fastq_files,test_list)
+    
+    
+if __name__ =='__main__':		# use this if you want to include modules from a subfolder
+        cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe() ))[0], r'Z:\ResearchHome\Groups\millergrp\home\common\Python\CAGE_Programs\NGS_one_main_program')))
+        if cmd_subfolder not in sys.path:
+            sys.path.insert(0, cmd_subfolder)
+            from clone_check_main_all_indels import search_fastq
+        main()
+    
